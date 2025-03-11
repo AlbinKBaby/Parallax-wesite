4ee57b2d79b39c06bfc8319cb8f326c11d5085f8
Albin K Baby <albinbaby06@gmail.com>
2025-03-12 00:29:44 +0530

    Edited

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let text = document.getElementById('text');
-let mountain = document.getElementById('mountain')
+let sky = document.getElementById('sky')
+let hill = document.getElementById('hill')
 let man = document.getElementById('man')
 
 window.addEventListener('scroll',() =>{
     let value = window.scrollY
-    text.style.marginTop = value * -1.5 + "px";
-    sky.style.top = value * -2.5 + "px";
-    hill2.style.top = value * -2.5 + "px";
-    man.style.top = value * 1.5 + "px";
-
+   
+    text.style.marginTop = value * 1 + "px";
+    sky.style.top = value * -2 + "px";
+    hill2.style.top = value * -1.5 + "px";
+    man.style.top = value * -2.5 + "px";
 })
